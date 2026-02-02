0dab97128275a6faf6afc506675cfd39d9d7b51f
Iván Torres <ivntorres89@gmail.com>
2026-02-02 12:47:44 +0100

    Removed exceed files

--- services/order-process/services/order-process/src/OrderProcess.Persistence.Abstractions/Repositories/Command/ICustomerCommandRepository.cs
+++
@@ -1,7 +0,0 @@
-using OrderProcess.Persistence.Abstractions.Entities;
-
-namespace OrderProcess.Persistence.Abstractions.Repositories.Command;
-
-public interface ICustomerCommandRepository : ICommandRepository<Customer>
-{
-}
