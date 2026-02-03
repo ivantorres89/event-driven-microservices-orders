c1f9f55482bdbb1e16c86f62fdbd3452b577ddde
Iván Torres <ivntorres89@gmail.com>
2026-02-03 22:54:40 +0100

    Added Unit tests support for FE, integrated in pipeline build + run tests for Frontend

---
+++ frontend/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // keep Jasmine Spec Runner output visible in browser
+        // (not used in CI)
+      },
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'text', file: 'coverage.txt' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true
+  });
+};
